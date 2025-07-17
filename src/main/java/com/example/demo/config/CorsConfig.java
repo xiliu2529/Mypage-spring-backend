420d6a08b95ff31ecfcc40d697929b09d551f3e5
xiliu <130113225+xiliu2529@users.noreply.github.com>
2025-07-18 02:04:57 +0900

    地址1

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -7,10 +7,13 @@ import org.springframework.web.servlet.config.annotation.*;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 允许所有路径
-                .allowedOrigins("http://localhost:5173") // 前端开发地址
-                .allowedMethods("*")    // 允许所有方法 GET, POST 等
-                .allowedHeaders("*")    // 允许所有请求头
-                .allowCredentials(true); // 允许携带 cookie
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://cool-baklava-1b8557.netlify.app"
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
